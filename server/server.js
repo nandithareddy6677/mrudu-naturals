5426a172a6ed8c8f936e025eb5ea77004bd52928
Nanditha reddy <nandithareddy@nandithas-MacBook-Air.local>
2026-06-09 23:29:34 -0700

    Fix gmail smtp

--- server/server.js
+++ server/server.js
@@ -22,7 +22,9 @@ console.log(
 );
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
